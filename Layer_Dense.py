765d55a063fe25b637e83876a544f2bf3bf60d67
sameer203 <mdsameerajuvath.com>
2022-01-25 16:36:17 +0530

    example problem

--- Layer_Dense.py
+++ Layer_Dense.py
@@ -1,3 +1,9 @@
+import numpy as np
+import nnfs 
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
 class Layer_Dense:
 
     def __init__(self, n_inputs, n_neurons):
@@ -8,3 +14,11 @@ class Layer_Dense:
     def forward(self,inputs):
         # Calculate output values from inputs, weights and biases
         self.output = np.dot(inputs, self.weights) + self.biases
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2, 3)
+
+dense1.forward(X)
+
+print(dense1.output[:5])
