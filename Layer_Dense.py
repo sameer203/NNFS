d67efb59ca9a6446684886be45ec943fff7e541d
sameer203 <mdsameerajuvath.com>
2022-01-25 16:16:18 +0530

    added self and biases

--- Layer_Dense.py
+++ Layer_Dense.py
@@ -1,8 +1,9 @@
 class Layer_Dense:
 
     def __init__(self, n_inputs, n_neurons):
-        pass
+        # initialize weights and biases
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros(1, n_neurons)
 
     def forward(self,inputs):
         pass
-    
