e2c4c4a1172b40808a054324d55258e46e5545c9
sameer203 <mdsameerajuvath.com>
2022-01-25 15:58:36 +0530

    matrix multiplication

--- neuron.py
+++ neuron.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-inputs = [1.0, 2.0, 3.0, 2.5]
+inputs = [[1.0, 2.0, 3.0, 2.5], 
+            [2.0, 5.0, -1.0, 2.0], 
+            [-1.5, 2.7, 3.3, 0.8]]
+
+
 weights = [[0.2, 0.8, -0.5, 1.0],
             [0.5, -0.91, 0.26, -0.5],
             [-0.26, -0.27, 0.17, 0.87]]
@@ -8,6 +12,7 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 
 bias = [2.0, 3.0, 0.5]
 
-layer_outputs = np.dot(weights, inputs) + bias
+layer_outputs = np.dot(inputs, np.array(weights).T) + bias
 
 print('the outputs are :', layer_outputs)
+
