63896406bfe9c943de2615abc25ce8413cadd4f6
sameer203 <mdsameerajuvath.com>
2022-01-25 16:49:56 +0530

    added forward pass with example_ sucess

--- Layer_Dense.py
+++ Layer_Dense.py
@@ -9,7 +9,7 @@ class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         # initialize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros(1, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
     def forward(self,inputs):
         # Calculate output values from inputs, weights and biases
@@ -17,7 +17,7 @@ class Layer_Dense:
 
 X, y = spiral_data(samples=100, classes=3)
 
-dense = Layer_Dense(2, 3)
+dense = Layer_Dense(2, 2)
 
 dense.forward(X)
 
