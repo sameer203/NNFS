1dad061bee0da9c7c81691a5cc2f1d2ad6bd92d8
sameer203 <mdsameerajuvath.com>
2022-01-26 18:57:36 +0530

    Softmax activation layer added

--- activation.py
+++ activation.py
@@ -1,7 +1,20 @@
 import numpy as np
 
-class Activation:
+class Activation_ReLU:
 
     def forward(self, inputs):
         # Calculate Output values from inputs
         self.output = np.maximum(0, inputs)
+
+
+class Activation_Softmax:
+
+    def forward(self,inputs):
+
+        # Get Normalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+
+        # Normalizethem for each samples
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+        self.output = probabilities
