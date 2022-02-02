54309647cfe8e3d725c91b27ba577c623e693856
sameer203 <mdsameerajuvath.com>
2022-02-02 16:52:44 +0530

    added back propogation to Dense layers

--- Layer_Dense.py
+++ Layer_Dense.py
@@ -15,6 +15,14 @@ class Layer_Dense:
         # Calculate output values from inputs, weights and biases
         self.output = np.dot(inputs, self.weights) + self.biases
 
+    def backward(self, dvalues):
+        # Gradient on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
 X, y = spiral_data(samples=100, classes=3)
 
 dense = Layer_Dense(2, 3)
