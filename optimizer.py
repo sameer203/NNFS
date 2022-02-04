926b64438275feb8327ee9fefab61a4c1a9d6af3
sameer203 <mdsameerajuvath.com>
2022-02-04 18:31:29 +0530

    added SGD optimizer with momemtum

--- optimizer.py
+++ optimizer.py
@@ -4,12 +4,40 @@ from nnfs.datasets import spiral_data
 
 class Optimizer_SGD:
 
-    def __init__(self, learning_rate = 0.1):
+    def __init__(self, learning_rate = 1, decay = 0):
         self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
 
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1 / (1 + self.decay * self.iterations))
+    
     def update_params(self, layers):
-        layers.weights += -self.learning_rate * layer.dweights
-        layer.biases += -self.learning_rate * layer.dbiases
+        if self.momentum:
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+
+                layer.bias_momentums = np.zeros_like(layer.biases)
+
+            weight_updates = self.momentum * layer.weight_momentums - self.current_learning_rate * layer.dweights
+            layer.weight_momentums = weight_updates
+
+            bias_updates = self.momentum * layer.bias_momentums - self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates
+
+        else:
+            weight_updates = - self.current_learning_rate * layer.dweights
+            bias_updates = - self.current_learning_rate * layer.dbiases
+
+        layers.weights += weight_updates
+        layer.biases += bias_updates
+
+    def post_update_params(self):
+        self.iterations += 1
+
+    
 
     
 
