f10189bb43ee92dbed9bd355316c48405130800f
sameer203 <mdsameerajuvath.com>
2022-02-04 19:19:23 +0530

    added RMSprop optimizer

--- optimizer.py
+++ optimizer.py
@@ -63,5 +63,30 @@ class Optimizer_AdaGrad:
     def post_update_params(self):
         self.iterations += 1   
 
-    
+class Optimizer_RMSprop:
 
+    def __init__(self, learning_rate = 1, decay = 0, eplison=1e-7, rho=0.9):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.eplison = eplison
+        self.rho = rho
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1 / (1 + self.decay * self.iterations))
+    
+    def update_params(self, layers):
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        layer.weight_cache = self.rho * layer.weight_cache + (1 - self.rho) * layer.dweights**2
+        layer.bias_cache = self.rho * layer.bias_cache + (1 - self.rho) * layer.dbiases**2
+
+        layer.weights += - self.current_learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.eplison)
+        layer.biases += - self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.eplison)
+
+    def post_update_params(self):
+        self.iterations += 1 
