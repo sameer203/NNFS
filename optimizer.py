d60bf5dc3947b7ba09238f51659f4a1a7772096f
sameer203 <mdsameerajuvath.com>
2022-02-04 19:34:29 +0530

    added Adam optimizer

--- optimizer.py
+++ optimizer.py
@@ -90,3 +90,44 @@ class Optimizer_RMSprop:
 
     def post_update_params(self):
         self.iterations += 1 
+
+class Optimizer_Adam:
+
+    def __init__(self, learning_rate=1, decay=0, eplison=1e-7, beta_1=0.9, beta_2=0.999):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.eplison = eplison
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1 / (1 + self.decay * self.iterations))
+    
+    def update_params(self, layers):
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_momentums = np.zeros_like(layer.weights)
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_momentums = np.zeros_like(layer.biases)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        layer.weight_momentums = self.beta_1 * layer.weight_momentums + (1 - self.beta_1) * layers.dweights
+        layer.bias_momentums = self.beta_1 * layer.bias_momentums + (1 - self.beta_1) * layers.dbias
+        
+        layer.weight_momentums_corrected = self.beta_1 * layer.weight_momentums + (1 - self.beta_1 ** (self.iterations + 1)) 
+        layer.bias_momentums_corrected = self.beta_1 * layer.bias_momentums + (1 - self.beta_1 ** (self.iterations + 1))
+
+        layer.weight_cache = self.beta_2* layer.weight_cache + (1 - self.beta_2) * layer.dweights**2
+        layer.bias_cache = self.beta_2* layer.bias_cache + (1 - self.beta_2) * layer.dbiases**2
+
+        layer.weight_cache_corrected = layer.weight_cache + (1 - self.beta_1 ** (self.iterations + 1)) 
+        layer.bias_cache_corrected = layer.bias_cache + (1 - self.beta_1 ** (self.iterations + 1))
+
+        layer.weights += - self.current_learning_rate * weight_momentums_corrected / (np.sqrt(weight_cache_corrected) + self.eplison)
+        layer.biases += - self.current_learning_rate * bias_momentums_corrected / (np.sqrt(bias_cache_corrected) + self.eplison)
+
+    def post_update_params(self):
+        self.iterations += 1 
