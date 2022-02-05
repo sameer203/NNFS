bb7fc7f8dc5c1678b634beb13ff427e9b5f8b971
sameer203 <mdsameerajuvath.com>
2022-02-05 12:38:09 +0530

    L1 and L2 regularization added

--- Layer_Dense.py
+++ Layer_Dense.py
@@ -6,10 +6,16 @@ nnfs.init()
 
 class Layer_Dense:
 
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs, n_neurons,
+                weight_regularizer_l1=0, weight_regularizer_l2=0, 
+                bias_regularizer_l1=0, bias_regularizer_l2=0):
         # initialize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        self.weight_regularizer_l1 = weight_regularizer_l1
+        self.weight_regularizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        self.bias_regularizer_l2 = bias_regularizer_l2
 
     def forward(self,inputs):
         # Calculate output values from inputs, weights and biases
--- loss.py
+++ loss.py
@@ -6,6 +6,21 @@ class Loss:
     # Calculates the data and regularization losses
     # given model output and ground truth values
 
+    def regularization_loss(self, layer):
+        regularization_loss = 0
+
+        if layer.weight_regularizer_l1 > 0:
+            regularization_loss += layer.weight_regularizer_l1 * np.sum(np.abs(layer.weights))
+        if layer.weight_regularizer_l2 > 0:
+            regularization_loss += layer.weight_regularizer_l2 * np.sum(layer.weights**2)
+
+        if layer.bias_regularizer_l1 > 0:
+            regularization_loss += layer.bias_regularizer_l1 * np.sum(np.abs(layer.biases))
+        if layer.bias_regularizer_l2 > 0:
+            regularization_loss += layer.bias_regularizer_l2 * np.sum(layer.biases**2)
+
+        return regularization_loss
+    
     def calculate(self, output, y):
 
         # Calculate sample loss
