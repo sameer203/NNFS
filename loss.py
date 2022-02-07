a8c4902d2acabadf8dc37730b71c586f5322ceaa
sameer203 <mdsameerajuvath.com>
2022-02-07 17:15:52 +0530

    Added MSE loss

--- loss.py
+++ loss.py
@@ -94,4 +94,19 @@ class Loss_BinaryCrossEntropy(Loss):
 
         self.dinputs = self.dinputs / samples
 
-    
+class Loss_MeanSquaredError(Loss):
+
+    def forward(self, y_pred, y_true):
+        sample_losses = np.mean((y_true - y_pred)**2, axis=-1)
+
+        return sample_losses
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        outputs = len(dvalues[0])
+
+        self.dinputs = -2 * (y_true - dvalues) / outputs
+
+        self.dinputs = self.dinputs / samples
+
+        
