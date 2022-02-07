d7893808f143efbbeda154213f7293927e612155
sameer203 <mdsameerajuvath.com>
2022-02-07 22:13:37 +0530

    Model class details added

--- model.py
+++ model.py
@@ -10,3 +10,35 @@ class Model():
     def add(self, layer):
         self.layers.append(layer)
 
+    def set(self, *, loss, optimizer):
+        self.loss = loss
+        self.optimizer = optimizer
+    
+    def finalize(self):
+        self.input_layer = Layer_Input()
+        layer_count = len(self.layers)
+
+        for i in range(layer_count):
+            if i == 0:
+                self.layers[i].prev = self.input_layer
+                self.layers[i].next = self.layers[i+1]
+            elif i < layer_count - 1:
+                self.layers[i].prev = self.layers[i-1]
+                self.layers[i].next = self.layers[i+1]
+            else:
+                self.layers[i].prev = self.layers[i-1]
+                self.layers[i].next = self.loss
+    
+    def train(self, X, y, *, epochs=1, print_every=1):
+        for epoch in range(0, epochs+1):
+            output = self.forward(X)
+            print(output)
+            exit()
+    
+    def forward(self, X):
+        self.input_layer.forward(X)
+        for layer in self.layers:
+            layer.forward(layer.prev.output)
+        return layer.output
+
+
