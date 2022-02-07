6608b4b07fa50ad011755cd106e341278dd0689d
sameer203 <mdsameerajuvath.com>
2022-02-07 15:41:36 +0530

    Added Dropout layer

---
+++ dropout.py
@@ -0,0 +1,17 @@
+import numpy as np
+import nnfs 
+from nnfs.datasets import spiral_data
+
+class Dropout():
+
+    def __init__(self, rate):
+        self.rate = 1 - rate
+    
+    def forward(self,inputs):
+        self.inputs = inputs
+        self.binary_mask = np.random.binomial(1, self.rate, size=input.shape) / self.rate
+        self.output = inputs * self.binary_mask
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues * self.binary_mask
+        
